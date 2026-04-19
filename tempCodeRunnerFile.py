5cc62c639abbaeaede2e884160832398563d9887
Nipun Ghiya <nipunghiya0941@gmail.com>
2026-04-19 21:16:10 +0530

    My Text To Audio Project

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from gtts import gTTS
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    audio_file = None
+
+    if request.method == "POST":
+        text = request.form["text"]
+        lang = request.form["lang"]
+        speed = request.form["speed"]
+
+        tts = gTTS(text=text, lang=lang, slow=(speed == "slow"))
+        audio_file = "static/output.mp3"
+        tts.save(audio_file)
+
+    return render_template("index.html", audio_file=audio_file)
+
+if __name__ == "__main__":
+    app.run(debug=True)
